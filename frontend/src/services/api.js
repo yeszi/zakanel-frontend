0e973f007bdd4c7eb4520f824c0ad5775f33751f
Nama GitHub Kamu <161846672+yeszi@users.noreply.github.com>
2026-08-11 06:48:15 +0700

    adalah

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-let baseURL = import.meta.env.VITE_API_BASE_URL || 'https://guards-fly-cia-frequencies.trycloudflare.com'
+let baseURL = import.meta.env.VITE_API_BASE_URL || 'https://deposits-rebecca-prize-chapel.trycloudflare.com'
 
 if (baseURL.endsWith('/')) {
   baseURL = baseURL.slice(0, -1)
