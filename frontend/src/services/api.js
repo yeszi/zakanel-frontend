b70b595d40cb59c180639a0dbfd9beab667d2fa4
Nama GitHub Kamu <161846672+yeszi@users.noreply.github.com>
2026-08-11 12:47:44 +0700

    perbaiki

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-let baseURL = import.meta.env.VITE_API_BASE_URL || 'https://amp-das-undefined-completing.trycloudflare.com'
+let baseURL = import.meta.env.VITE_API_BASE_URL || 'https://chocolate-group-program-polished.trycloudflare.com'
 
 if (baseURL.endsWith('/')) {
   baseURL = baseURL.slice(0, -1)
